6c06d40969f9326335971f3847ad07d1c2e5b1ea
imroh17kadam <kadam17rohit@gmail.com>
2026-02-27 11:10:45 +0530

    Imports corrected for dag

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -5,10 +5,10 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-from airflow.deployment.extract import DataExtractor
-from airflow.deployment.load_raw_to_snowflake import load_raw_to_snowflake
-from airflow.deployment.transform import transform_data
-from airflow.deployment.load import load_data
+from airflow.dag_tasks.extract import DataExtractor
+from airflow.dag_tasks.load_raw_to_snowflake import load_raw_to_snowflake
+from airflow.dag_tasks.transform import transform_data
+from airflow.dag_tasks.load import load_data
 
 # Datadog monitoring
 # from plugins.datadog_monitoring import track_task
